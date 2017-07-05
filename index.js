5a8093f781d7e405d3e75a1e855bb8d127c82839
Takkar <www.shtakkar@gmail.com>
2017-07-05 21:37:21 +0530

    Changed code to es5

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
-import $ from 'jquery'
+var $ = require('jquery');
 
 // FUNCTION TO NOTIFY
-const Notify = options => {
-  let defaults = {
+var Notify = function(options){
+  var defaults = {
     beforeTop: "105%",     // No need to overwrite this
     afterTop:  "90%",      // same here, no overwriting
     value:     "Message",  // This will be the message
     selector:  null,       // Selector you just created
     action:    null,       // responsible to redirect to a URL when clicked
-    done:      () => { return }       // It's a function which will be executed when you're notified
+    done:      function(){ return; }       // It's a function which will be executed when you're notified
   };
 
-  let settings = { ...defaults, ...options }
+  var settings = Object.assign({}, defaults, options);
   
-  // var 
-  //   selector  = settings.selector,
-  //   value     = settings.value,
-  //   beforeTop = settings.beforeTop,
-  //   afterTop  = settings.afterTop,
-  //   action    = settings.action,
-  //   done      = settings.done;
+  var 
+    selector  = settings.selector,
+    value     = settings.value,
+    beforeTop = settings.beforeTop,
+    afterTop  = settings.afterTop,
+    action    = settings.action,
+    done      = settings.done;
 
-  let { selector, value, beforeTop, afterTop, action, done } = settings
-
-  $(selector).find('span').html(value)
+  $(selector).find('span').html(value);
 
   $(selector).animate({
     top: afterTop
-  }, "fast",  () => {
-    done()
-    setTimeout(() => {
-      $(selector).animate({ top: beforeTop })
-    }, 3000)
-  })
+  }, "fast",  function(){
+    done();
+    setTimeout(function(){
+      $(selector).animate({ top: beforeTop });
+    }, 3000);
+  });
 
-  $(selector).on('click', () => {
-    action != null ? location.href = action : null
-    $(selector).animate({ top: beforeTop })
+  $(selector).on('click', function(e){
+    action != null ? location.href = action : null;
+    $(selector).animate({ top: beforeTop });
   })
 
 }
 
-export default Notify
+module.exports = Notify;
