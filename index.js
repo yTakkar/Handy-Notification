52994f0951bf533dbc9ccf0be20c726ca9dcf31a
yTakkar <www.shtakkar@gmail.com>
2018-02-28 12:30:39 +0530

    Some minor changes

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const $ = require('jquery')
 // FUNCTION TO NOTIFY
 const Notify = function(options){
   const defaults = {
-    beforeTop: "105%",     // No need to overwrite this
-    afterTop:  "90%",      // same here, no overwriting
-    value:     "Message",  // This will be the message
+    beforeTop: '105%',     // No need to overwrite this
+    afterTop:  '90%',      // same here, no overwriting
+    value:     'Message',  // This will be the message
     selector:  $('.handy-notify'),       // Selector you just created
     action:    null,       // responsible to redirect to a URL when clicked
     done:      function(){ return }       // It's a function which will be executed when you're notified
@@ -23,23 +23,23 @@ const Notify = function(options){
 
   $(selector)
     .css({
-        "position": "fixed",
-        "background": "#333",
-        "left": "2%",
-        "color": "white",
-        "border-radius": "3px",
-        "padding": "12px 80px 12px 25px",
-        "font-size": "15px",
-        "cursor": "pointer",
-        "text-align": "left",
-        "z-index": "30000",
-        "top": "105%"
+      'position': 'fixed',
+      'background': '#333',
+      'left': '2%',
+      'color': 'white',
+      'border-radius': '3px',
+      'padding': '12px 80px 12px 25px',
+      'font-size': '15px',
+      'cursor': 'pointer',
+      'text-align': 'left',
+      'z-index': '30000',
+      'top': '105%'
     })
     .find('span').html(value)
 
   $(selector).animate({
     top: afterTop
-  }, "fast",  function(){
+  }, 'fast',  function(){
     done()
     setTimeout(function(){
       $(selector).animate({ top: beforeTop })
