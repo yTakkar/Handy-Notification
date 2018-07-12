c841315510fa3e9bf3af05debd9b72088b4a0101
yTakkar <www.shtakkar@gmail.com>
2018-07-12 22:48:42 +0530

    Minor changes

--- example/main.js
+++ example/main.js
@@ -1,11 +1,7 @@
 import Notify from '../index'
 
 Notify({
-    value: 'Hey, how are you doing??',
-    onClick: () => {
-        location.href = 'https://github.com/yTakkar/Handy-Notification'
-    },
-    done: () => {
-        console.log("I'm back!!")
-    }
+  value: 'Hey, how are you doing??',
+  onClick: () => location.href = 'https://github.com/yTakkar/Handy-Notification',
+  done: () => console.log("I'm back!!")
 })
